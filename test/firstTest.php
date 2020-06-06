6388d857fbf07312cfe46ec6c9800de4f25aefb5
Andrés Kouvach <akouvach@yahoo.com>
2020-06-06 08:40:01 -0300

    agregué phpUnit

---
+++ test/firstTest.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+use App\Calculator;
+
+
+class FirstTest extends TestCase {
+
+    public function testSuma() 
+    {
+        $c = new Calculator();
+        $this->assertEquals(5, $c->suma(3,2));
+
+
+    }
+
+}
+
+
+?>
